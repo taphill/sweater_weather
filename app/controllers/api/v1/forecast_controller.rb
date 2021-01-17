4967dd95ba816709732e268e43695f0f67855059
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-17 13:08:26 -0600

    Add happy and sad path testing to for forecasts request

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -5,9 +5,21 @@ module Api
     class ForecastController < ApplicationController
       def index
         geocode = GeocodingFacade.latitude_longitude(params[:location])
-        forecast = WeatherFacade.forecast(latitude: geocode[:latitude], longitude: geocode[:longitude])
 
-        render json: ForecastSerializer.new(forecast)
+        if location_valid?(geocode[:quality_code])
+          forecast = WeatherFacade.forecast(latitude: geocode[:latitude], longitude: geocode[:longitude])
+          render json: ForecastSerializer.new(forecast)
+        else
+          render json: { message: 'Please ensure you entered a valid location' }, status: 404
+        end
+      end
+
+      private
+
+      def location_valid?(code)
+        return false if code == 'A1XAX'
+
+        true
       end
     end
   end
--- app/facades/geocoding_facade.rb
+++ app/facades/geocoding_facade.rb
@@ -6,7 +6,8 @@ class GeocodingFacade
 
     latitude = json_body[:results].first[:locations].first[:latLng][:lat]
     longitude = json_body[:results].first[:locations].first[:latLng][:lng]
+    quality_code = json_body[:results].first[:locations].first[:geocodeQualityCode]
 
-    { latitude: latitude, longitude: longitude }
+    { latitude: latitude, longitude: longitude, quality_code: quality_code }
   end
 end
