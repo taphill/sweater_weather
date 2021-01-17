88abb860af3046628f8dae34d8436ee22c5ef14f
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-17 15:08:54 -0600

    Create Image poro

---
+++ app/poros/image.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Image
+  attr_reader :photographer, :profile, :image_location, :image_url
+
+  def initialize(attr:, location:)
+    @photographer = attr[:results].first[:user][:name]
+    @profile = attr[:results].first[:user][:links][:html]
+    @image_location = location
+    @image_url = attr[:results].first[:urls][:raw]
+  end
+end
