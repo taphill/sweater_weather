c08cb06b0f5ae1fc2b2b7b5ed7cc04830bfd69bb
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-17 15:09:19 -0600

    Refactor Backgrounds controller

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,17 +1,13 @@
-class Api::V1::BackgroundsController < ApplicationController
-  def index
-    conn = Faraday.new(
-      url: 'https://api.unsplash.com',
-      params: { client_id: Rails.application.credentials.unsplash[:api_key] },
-      headers: { 'Content-Type' => 'application/json', 'Accept-Version' => 'v1' }
-    )
+# frozen_string_literal: true
 
-    response = conn.get('/search/photos') do |req|
-      req.params['query'] = "#{params[:location]} city skyline"
-      req.params['per_page'] = 1
+module Api
+  module V1
+    class BackgroundsController < ApplicationController
+      def index
+        image = ImageFacade.background_image(params[:location])
+
+        render json: ImageSerializer.new(image)
+      end
     end
-
-    json_body = JSON.parse(response.body, symbolize_names: true)
-require 'pry'; binding.pry
   end
 end
--- spec/requests/api/v1/backgrounds_request_spec.rb
+++ spec/requests/api/v1/backgrounds_request_spec.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Api/V1/Backgrounds request", type: :request do
+RSpec.describe 'Api/V1/Backgrounds request', type: :request do
   describe 'GET /api/v1/backgrounds?location=', :vcr do
     let(:json_body) { JSON.parse(response.body, symbolize_names: true) }
 
     before { get api_v1_backgrounds_path, params: { location: 'denver,co' } }
 
-    it { expect(response.status).to eq(200) }
+    it 'has data' do
+    end
   end
 end
