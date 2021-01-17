118b49e4ff69d9cbbfb2393bf8d9e60085a1a163
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-17 15:08:19 -0600

    Create Image service

---
+++ app/services/image_service.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class ImageService
+  def self.conn
+    Faraday.new(
+      url: 'https://api.unsplash.com',
+      params: { client_id: Rails.application.credentials.unsplash[:api_key] },
+      headers: { 'Content-Type' => 'application/json', 'Accept-Version' => 'v1' }
+    )
+  end
+
+  def self.background_image_data(location)
+    response = conn.get('/search/photos') do |req|
+      req.params['query'] = "#{location} city skyline"
+      req.params['per_page'] = 1
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private_class_method :conn
+end
