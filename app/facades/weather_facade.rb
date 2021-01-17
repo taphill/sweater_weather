0d5c7cb9f9909cd7085d8a41dee41af0493c1897
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-16 18:57:07 -0600

    Add weather methods to Weather facade

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,5 +1,25 @@
 class WeatherFacade
   def self.weather_data(latitude:, longitude:)
-    data = WeatherService.weather_data(latitude, longitude) 
+    data = WeatherService.weather_data(latitude: latitude, longitude: longitude) 
+
+    Forecast.new(
+      current_weather: CurrentWeather.new(data),
+      daily_weather: daily_weather(data),
+      hourly_weather: hourly_weather(data)
+    )
+  end
+
+  def self.daily_weather(data)
+    data[:daily][0..4].map do |day|
+      DailyWeather.new(day) 
+    end
+  end
+
+  def self.hourly_weather(data)
+    x = data[:hourly].map do |hour|
+      HourlyWeather.new(hour)
+    end
+
+require 'pry'; binding.pry
   end
 end
