6389ec857d125ade6373feae01b0e65496a8d01a
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-18 16:21:09 -0600

    Rename MapService location method to geocode

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -3,14 +3,14 @@
 class MapService
   def self.conn
     Faraday.new(
-      url: 'http://www.mapquestapi.com/geocoding/v1/',
+      url: 'http://www.mapquestapi.com',
       params: { key: Rails.application.credentials.mapquest[:api_key] },
       headers: { 'Content-Type' => 'application/json' }
     )
   end
 
-  def self.location(location)
-    response = conn.get('address') do |request|
+  def self.geocode(location)
+    response = conn.get('geocoding/v1/address') do |request|
       request.params['location'] = location
       request.params['maxResults'] = 1
     end
@@ -19,6 +19,11 @@ class MapService
   end
 
   def self.route(origin:, destination:)
+    response = conn.get('directions/v2/routematrix') do |request|
+      request.body = { locations: [origin, destination] }.to_json
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   private_class_method :conn
