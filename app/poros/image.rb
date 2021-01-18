ff364ac1eb1cc16fa5ac19d5f1af38e439209382
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-17 18:36:45 -0600

    Update image poro

--- app/poros/image.rb
+++ app/poros/image.rb
@@ -6,7 +6,7 @@ class Image
   def initialize(attr:, location:)
     @photographer = attr[:results].first[:user][:name]
     @profile = attr[:results].first[:user][:links][:html]
-    @hosting_site = "https://www.unsplash.com"
+    @hosting_site = 'https://www.unsplash.com'
     @image_location = location
     @image_url = attr[:results].first[:urls][:raw]
   end
