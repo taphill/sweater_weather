e0449131fde5c8d2c6a282fd1dbc4ca92f5f99e4
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-17 18:36:33 -0600

    Rubocop files

--- spec/requests/api/v1/backgrounds_request_spec.rb
+++ spec/requests/api/v1/backgrounds_request_spec.rb
@@ -9,6 +9,30 @@ RSpec.describe 'Api/V1/Backgrounds request', type: :request do
     before { get api_v1_backgrounds_path, params: { location: 'denver,co' } }
 
     it 'has data' do
+      expect(json_body).to be_a(Hash)
+      expect(json_body).to have_key(:data)
+      expect(json_body[:data]).to be_a(Hash)
+      expect(json_body[:data]).to have_key(:id)
+      expect(json_body[:data][:id]).to be_nil
+      expect(json_body[:data]).to have_key(:type)
+      expect(json_body[:data][:type]).to eq('image')
+      expect(json_body[:data]).to have_key(:attributes)
+      expect(json_body[:data][:attributes]).to be_a(Hash)
+      expect(json_body[:data][:attributes]).to have_key(:image)
+      expect(json_body[:data][:attributes][:image]).to be_a(Hash)
+    end
+
+    it 'has attributes' do
+      expect(json_body[:data][:attributes][:image]).to have_key(:photographer)
+      expect(json_body[:data][:attributes][:image][:photographer]).to be_a(String)
+      expect(json_body[:data][:attributes][:image]).to have_key(:profile)
+      expect(json_body[:data][:attributes][:image][:profile]).to be_a(String)
+      expect(json_body[:data][:attributes][:image]).to have_key(:hosting_site)
+      expect(json_body[:data][:attributes][:image][:hosting_site]).to be_a(String)
+      expect(json_body[:data][:attributes][:image]).to have_key(:image_location)
+      expect(json_body[:data][:attributes][:image][:image_location]).to be_a(String)
+      expect(json_body[:data][:attributes][:image]).to have_key(:image_url)
+      expect(json_body[:data][:attributes][:image][:image_url]).to be_a(String)
     end
   end
 end
