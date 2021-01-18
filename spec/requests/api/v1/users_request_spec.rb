672dd4fc93b744cb8ca6e7721154ef3b3e1bc3c0
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-17 20:00:38 -0600

    Add user controller create action

---
+++ spec/requests/api/v1/users_request_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Api/V1/Users request', type: :request do
+  describe 'POST /api/v1/users' do
+    let(:json_body) { JSON.parse(response.body, symbolize_names: true) }
+
+    context 'when valid' do
+      before { 
+        post api_v1_users_path,
+        params: { email: 'hi@example.com', password: 'password', password_confirmation: 'password' }
+      }
+
+      it { expect(response.status).to eq(201) }
+
+      it { expect(json_body).to be_a(Hash) }
+      it { expect(json_body).to have_key(:data) }
+      it { expect(json_body[:data]).to be_a(Hash) }
+      it { expect(json_body[:data]).to have_key(:id) }
+      it { expect(json_body[:data][:id]).to eq(User.all.first.id.to_s) }
+      it { expect(json_body[:data]).to have_key(:type) }
+      it { expect(json_body[:data][:type]).to eq('user') }
+      it { expect(json_body[:data]).to have_key(:attributes) }
+      it { expect(json_body[:data][:attributes]).to be_a(Hash) }
+
+      it { expect(json_body[:data][:attributes]).to have_key(:email) }
+      it { expect(json_body[:data][:attributes][:email]).to eq(User.all.first.email) }
+      it { expect(json_body[:data][:attributes]).to have_key(:api_key) }
+      it { expect(json_body[:data][:attributes][:api_key]).to eq(User.all.first.api_key) }
+    end
+  end
+end
