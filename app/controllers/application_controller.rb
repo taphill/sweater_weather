459d96464b708c0e2d0f4cd2167cec8542fc365e
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-17 19:59:46 -0600

    Add invalid record error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+  private
+
+  def record_invalid(error)
+    render json: { message: error.message }, status: :forbidden
+  end
 end
