97677425c60a3a8a3d6b62ec8be4d63ab39c7efe
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-19 11:28:37 -0600

    wip: MunchiesFacade

--- app/facades/munchies_facade.rb
+++ app/facades/munchies_facade.rb
@@ -4,20 +4,30 @@ class MunchiesFacade
   def self.create_munchie(start:, end_location:, food:)
     route_data = MapService.munchies_route(from: start, to: end_location)
 
-    Munchie.new(
-      destination: end_location,
-      travel_time: travel_time
-      forecast: 
-      restaurant:
-    )
+require 'pry'; binding.pry
+    # Munchie.new(
+    #   destination: end_location,
+    #   travel_time: seconds_to_hours(route_data[:route][:realTime]),
+    #   forecast: 
+    #   restaurant:
+    # )
   end
 
-  private
-
-  def seconds_to_hours(seconds)
+  def self.seconds_to_hours(seconds)
     hours = seconds / 3600.0
     minutes = (hours % 1) * 60
 
     "#{hours.floor} hours, #{minutes.round} minutes"
   end
+
+  def self.current_forecast(location)
+  end
+
+  def self.restaurant(location:, term:, open_at:)
+    data = RestaurantService.find(location: location, term: term, open_at: open_at)
+
+require 'pry'; binding.pry
+  end
+
+  private_class_method :seconds_to_hours
 end
