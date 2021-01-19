98459cef29ef6acccef41a2a9c7f88d953124b73
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-19 12:36:01 -0600

    Render munchies json in MunchiesController index action

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -4,7 +4,13 @@ module Api
   module V1
     class MunchiesController < ApplicationController
       def index
+        munchie = MunchiesFacade.create_munchie(
+          start: params[:start],
+          end_location: params[:end_location],
+          food: params[:food]
+        )
 
+        render json: MunchieSerializer.new(munchie)
       end
     end
   end
