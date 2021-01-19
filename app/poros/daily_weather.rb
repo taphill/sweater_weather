2e430546daf9413f1e9ae77e4548eb838626886f
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-19 08:55:14 -0600

    Update time attributes in weather poros

--- app/poros/daily_weather.rb
+++ app/poros/daily_weather.rb
@@ -4,9 +4,9 @@ class DailyWeather
   attr_reader :date, :sunrise, :sunset, :max_temp, :min_temp, :conditions, :icon
 
   def initialize(attr)
-    @date = Time.zone.at(attr[:dt]).to_date.to_s
-    @sunrise = Time.zone.at(attr[:sunrise]).to_s
-    @sunset = Time.zone.at(attr[:sunset]).to_s
+    @date = Time.zone.at(attr[:dt]).getlocal.to_date.to_s
+    @sunrise = Time.zone.at(attr[:sunrise]).getlocal.to_s
+    @sunset = Time.zone.at(attr[:sunset]).getlocal.to_s
     @max_temp = attr[:temp][:max]
     @min_temp = attr[:temp][:min]
     @conditions = attr[:weather].first[:description]
