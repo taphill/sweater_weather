ed2e0e807143ca589442b6cd4185aedab11105d2
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-19 17:46:52 -0600

    Create Image poro spec

---
+++ spec/poros/image_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Image, type: :poro do
+  describe 'attributes', :vcr do
+    let(:data) { ImageService.background_image_data('denver,co') }
+    let(:image) { Image.new(attr: data, location: 'denver,co') }
+
+    it { expect(image).to be_a(Image) }
+
+    it { expect(image.photographer).to be_a(String) }
+    it { expect(image.profile).to be_a(String) }
+    it { expect(image.hosting_site).to be_a(String) }
+    it { expect(image.image_location).to be_a(String) }
+    it { expect(image.image_url).to be_a(String) }
+  end
+end
