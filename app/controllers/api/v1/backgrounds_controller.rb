086e41420eafabbe6fef5eb5f5d5dae7419fc761
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-19 18:37:22 -0600

    Add error handling to BackgroundsController

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -4,9 +4,22 @@ module Api
   module V1
     class BackgroundsController < ApplicationController
       def index
-        image = ImageFacade.background_image(params[:location])
+        geocode = GeocodingFacade.latitude_longitude(params[:location])
 
-        render json: ImageSerializer.new(image)
+        if location_valid?(geocode[:quality_code])
+          image = ImageFacade.background_image(params[:location])
+          render json: ImageSerializer.new(image)
+        else
+          render json: { message: 'Please ensure you entered a valid location' }, status: :not_found
+        end
+      end
+
+      private
+
+      def location_valid?(code)
+        return false if code == 'A1XAX'
+
+        true
       end
     end
   end
