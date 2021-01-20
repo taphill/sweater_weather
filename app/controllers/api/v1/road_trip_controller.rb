c3c9e80d99fbf452a1e488e558a5a261d8b41c78
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-19 19:59:23 -0600

    Refactor RoadTripController create action

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -4,24 +4,28 @@ module Api
   module V1
     class RoadTripController < ApplicationController
       def create
-        unless location_valid?
-          return render json: { message: "Please ensure you've entered valid locations" }, status: :not_found
-        end
-
         if User.find_by(api_key: params[:api_key])
-          road_trip = RoadTripFacade.trip(origin: params[:origin], destination: params[:destination])
-
-          if road_trip.nil?
-            render json: { message: 'We are unable to route with the given locations.' }, status: :unprocessable_entity
-          else
-            render json: RoadTripSerializer.new(road_trip)
-          end
+          render_road_trip
         else
           render json: { message: 'Unauthorized request' }, status: :unauthorized
         end
       end
 
       private
+
+      def render_road_trip
+        unless location_valid?
+          return render json: { message: "Please ensure you've entered valid locations" }, status: :not_found
+        end
+
+        road_trip = RoadTripFacade.trip(origin: params[:origin], destination: params[:destination])
+
+        if road_trip.nil?
+          render json: { message: 'We are unable to route with the given locations.' }, status: :unprocessable_entity
+        else
+          render json: RoadTripSerializer.new(road_trip)
+        end
+      end
 
       def location_valid?
         return false if number?
